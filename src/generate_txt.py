a28b10bc44c74b508cac545b263e2f2740a1f092
melofmemo <melofmemory@gmail.com>
2025-05-18 08:24:47 +0900

    Generate Seoul time

--- src/generate_txt.py
+++ src/generate_txt.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def main():
     os.makedirs("results", exist_ok=True)
+    now = datetime.now(ZoneInfo("Asia/Seoul"))
     with open(os.path.join("results", "now.txt"), "w") as f:
-        f.write(datetime.now().isoformat() + "\n")
+        f.write(now.isoformat() + "\n")
 
 if __name__ == "__main__":
     main()
